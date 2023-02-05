af9493e3915a314d8d477f917d1da2d6eebf941f
Abhishek Tomar <abhishek.tomar@jeevlifeworks.com>
2023-02-05 22:19:26 +0530

    added

---
+++ spring-batching-to-scv-database/src/test/java/com/jeevlifeworks/com/SpringBatchingToScvDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeevlifeworks.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchingToScvDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
