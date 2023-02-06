7af7a4b9bfdccbafbacd35980e3b589a715ebd1c
Abhishek Tomar <abhishek.tomar@jeevlifeworks.com>
2023-02-06 16:41:50 +0530

    add

--- spring-batching-to-scv-database/src/main/java/com/jeevlifeworks/com/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jeevlifeworks.com.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jeevlifeworks.com.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
